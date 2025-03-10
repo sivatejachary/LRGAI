7f23a586090bed95f70f5e0ff981a79c170d01cc
JAYAVARAPU SIVA TEJACHARY <j.shivachary@gmail.com>
2025-03-10 13:03:39 +0530

    Update tokenizers.py

--- lrgaimodel/tokenizers.py
+++ lrgaimodel/tokenizers.py
@@ -7,10 +7,13 @@ class Tokenizer:
     def __init__(self):
         self.tokenizer = HFTokenizer(BPE())
         self.tokenizer.pre_tokenizer = Whitespace()
+
     def train(self, files):
         trainer = BpeTrainer(special_tokens=["<unk>", "<s>", "</s>"])
         self.tokenizer.train(files, trainer)
+
     def encode(self, text):
-        return self.tokenizer.encode(text).ids
-    def decode(self, ids):
-        return self.tokenizer.decode(ids)
+        tokens = self.tokenizer.encode(text).ids
+        if len(tokens) == 0:  # Ensure at least one token
+            tokens = [0]  
+        return tokens
