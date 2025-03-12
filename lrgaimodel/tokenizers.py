2dca0ccd1407417c308b26caa4812673e7eb225c
JAYAVARAPU SIVA TEJACHARY <j.shivachary@gmail.com>
2025-03-12 15:06:25 +0530

    Update tokenizers.py

--- lrgaimodel/tokenizers.py
+++ lrgaimodel/tokenizers.py
@@ -1,19 +1,35 @@
-from tokenizers import Tokenizer as HFTokenizer
-from tokenizers.models import BPE
-from tokenizers.trainers import BpeTrainer
-from tokenizers.pre_tokenizers import Whitespace
+import os
+from logging import getLogger
+from typing import List
+
+from sentencepiece import SentencePieceProcessor
+
+
+logger = getLogger()
+
 
 class Tokenizer:
-    def __init__(self):
-        self.tokenizer = HFTokenizer(BPE())
-        self.tokenizer.pre_tokenizer = Whitespace()
+    def __init__(self, model_path: str):
+        assert os.path.isfile(model_path), model_path
+        self.sp_model = SentencePieceProcessor(model_file=model_path)
+        logger.info(f"Reloaded SentencePiece model from {model_path}")
 
-    def train(self, files):
-        trainer = BpeTrainer(special_tokens=["<unk>", "<s>", "</s>"])
-        self.tokenizer.train(files, trainer)
+        self.n_words: int = self.sp_model.vocab_size()
+        self.bos_id: int = self.sp_model.bos_id()
+        self.eos_id: int = self.sp_model.eos_id()
+        self.pad_id: int = self.sp_model.pad_id()
+        logger.info(
+            f"#words: {self.n_words} - BOS ID: {self.bos_id} - EOS ID: {self.eos_id}"
+        )
+        assert self.sp_model.vocab_size() == self.sp_model.get_piece_size()
 
-    def encode(self, text):
-        tokens = self.tokenizer.encode(text).ids
-        if len(tokens) == 0:  # Ensure at least one token
-            tokens = [0]  
-        return tokens
+    def encode(self, s: str, bos: bool, eos: bool) -> List[int]:
+        t = self.sp_model.encode(s)
+        if bos:
+            t = [self.bos_id] + t
+        if eos:
+            t = t + [self.eos_id]
+        return t
+
+    def decode(self, t: List[int]) -> str:
+        return self.sp_model.decode(t)
