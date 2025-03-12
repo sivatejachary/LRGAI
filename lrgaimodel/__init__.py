00583a0ca52b69771b87c1f19c355b68121210af
JAYAVARAPU SIVA TEJACHARY <j.shivachary@gmail.com>
2025-03-12 15:05:14 +0530

    Update __init__.py

--- lrgaimodel/__init__.py
+++ lrgaimodel/__init__.py
@@ -1,15 +1,3 @@
-from .model import LRGAIModel
-from .tokenizers import Tokenizer
-from .retrieval import Retriever
-from .generation import Generator
-
-def load_model():
-    global _model, _tokenizer, _retriever, _generator
-    if _model is None:
-        _tokenizer = Tokenizer()
-        _retriever = Retriever()
-        _model = LRGAIModel()
-        _generator = Generator(_model, _retriever)
-    return _model, _tokenizer, _retriever, _generator
-
-__all__ = ["LRGAIModel", "Tokenizer", "Retriever", "Generator", "load_model"]
+from .generation import LAM, Dialog
+from .model import ModelArgs, Transformer
+from .tokenizer import Tokenizer
